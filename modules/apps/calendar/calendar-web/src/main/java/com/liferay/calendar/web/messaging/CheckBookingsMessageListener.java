a521de519c337f7003be5b1fc2a73d280652dcbe
tina.tian <tina.tian@liferay.com>
2015-11-04 22:40:58 +0800

    LPS-59489 Scheduler job is not running since compoent service is not activated

--- modules/apps/calendar/calendar-web/src/main/java/com/liferay/calendar/web/messaging/CheckBookingsMessageListener.java
+++ modules/apps/calendar/calendar-web/src/main/java/com/liferay/calendar/web/messaging/CheckBookingsMessageListener.java
@@ -35,7 +35,7 @@ import org.osgi.service.component.annotations.Reference;
  * @author Fabio Pezzutto
  * @author Eduardo Lundgren
  */
-@Component(service = CheckBookingsMessageListener.class)
+@Component(immediate = true, service = CheckBookingsMessageListener.class)
 public class CheckBookingsMessageListener
 	extends BaseSchedulerEntryMessageListener {
 
