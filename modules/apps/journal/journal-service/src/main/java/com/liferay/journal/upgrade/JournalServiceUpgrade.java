a7403f446d756d668a507a44263fa6d3b1d9bfe4
tina.tian <tina.tian@liferay.com>
2015-11-04 16:57:02 +0800

    LPS-59489 Register service since a scheduler job(CheckArticleMessageListener) depends on it.

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/upgrade/JournalServiceUpgrade.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/upgrade/JournalServiceUpgrade.java
@@ -40,7 +40,7 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Eduardo Garcia
  */
-@Component(immediate = true)
+@Component(immediate = true, service = JournalServiceUpgrade.class)
 public class JournalServiceUpgrade implements UpgradeStepRegistrator {
 
 	@Override
