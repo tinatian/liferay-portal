6af8bd0b4dd0589021f6accebe542a07b7a72f9f
tina.tian <tina.tian@liferay.com>
2015-12-03 19:20:16 +0800

    LPS-60923 Destroy destination after it is unregistered

--- modules/portal/portal-scheduler/src/main/java/com/liferay/portal/scheduler/internal/SchedulerEngineHelperImpl.java
+++ modules/portal/portal-scheduler/src/main/java/com/liferay/portal/scheduler/internal/SchedulerEngineHelperImpl.java
@@ -808,10 +808,12 @@ public class SchedulerEngineHelperImpl implements SchedulerEngineHelper {
 
 	@Deactivate
 	protected void deactivate() {
+		if (_bundleContext == null) {
+			return;
+		}
+
 		if (_serviceTracker != null) {
 			_serviceTracker.close();
-
-			_serviceTracker = null;
 		}
 
 		try {
@@ -830,7 +832,12 @@ public class SchedulerEngineHelperImpl implements SchedulerEngineHelper {
 		for (ServiceRegistration<Destination> serviceRegistration :
 				_destinationServiceRegistrations) {
 
+			Destination destination = _bundleContext.getService(
+				serviceRegistration.getReference());
+
 			serviceRegistration.unregister();
+
+			destination.destroy();
 		}
 
 		for (ServiceRegistration<SchedulerEventMessageListener>
