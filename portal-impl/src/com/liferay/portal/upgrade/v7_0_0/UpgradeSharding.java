f6d01019e8c24105562a934c7423e5b9ad78c8cb
tina.tian <tina.tian@liferay.com>
2016-02-26 15:11:22 +0800

    LPS-63617 Apply LoggingTimer, com.liferay.portal.upgrade.v7_0_0

--- portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeSharding.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeSharding.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.upgrade.util.UpgradeTable;
 import com.liferay.portal.kernel.upgrade.util.UpgradeTableFactoryUtil;
 import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.LoggingTimer;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.upgrade.v7_0_0.util.ClassNameTable;
@@ -66,24 +67,29 @@ public class UpgradeSharding extends UpgradeProcess {
 	protected void copyControlTables(List<String> shardNames) throws Exception {
 		boolean defaultPartitioningEnabled = false;
 
-		List<String> uniqueShardNames = ListUtil.unique(shardNames);
+		try (LoggingTimer loggingTimer = new LoggingTimer()) {
+			List<String> uniqueShardNames = ListUtil.unique(shardNames);
 
-		if (uniqueShardNames.size() < shardNames.size()) {
-			defaultPartitioningEnabled = true;
-		}
+			if (uniqueShardNames.size() < shardNames.size()) {
+				defaultPartitioningEnabled = true;
+			}
 
-		String defaultShardName = PropsUtil.get("shard.default.name");
+			String defaultShardName = PropsUtil.get("shard.default.name");
 
-		if (!defaultPartitioningEnabled && Validator.isNull(defaultShardName)) {
-			throw new RuntimeException(
-				"The property \"shard.default.name\" is not set in " +
-					"portal.properties. Please specify a default shard name " +
-						"from: " + StringUtil.merge(shardNames, ", ") + ".");
-		}
+			if (!defaultPartitioningEnabled &&
+				Validator.isNull(defaultShardName)) {
 
-		for (String uniqueShardName : uniqueShardNames) {
-			if (!uniqueShardName.equals(defaultShardName)) {
-				copyControlTables(uniqueShardName);
+				throw new RuntimeException(
+					"The property \"shard.default.name\" is not set in " +
+						"portal.properties. Please specify a default shard " +
+							"name from: " + StringUtil.merge(shardNames, ", ") +
+								".");
+			}
+
+			for (String uniqueShardName : uniqueShardNames) {
+				if (!uniqueShardName.equals(defaultShardName)) {
+					copyControlTables(uniqueShardName);
+				}
 			}
 		}
 	}
