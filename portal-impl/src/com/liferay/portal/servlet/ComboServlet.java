6686f948db6e621a300781bcb87ab96144375655
Tina Tian <tina.tian@liferay.com>
2016-05-17 12:25:57 +0800

    LPS-65896 Apply dynamic portal cache, retrieve portal cache form MultiVMPool/SingleVMPool each time it is invoked, so that when MultiVMPool/SingleVMPool services are changed, the lastest portal cache is retrieved instead of the stealed one. This should only be applied to portal cache which is not used in OSGi bundle, since those using in bundles can be refreshed by service dependecy logic.

--- portal-impl/src/com/liferay/portal/servlet/ComboServlet.java
+++ portal-impl/src/com/liferay/portal/servlet/ComboServlet.java
@@ -450,9 +450,9 @@ public class ComboServlet extends HttpServlet {
 	private static final Log _log = LogFactoryUtil.getLog(ComboServlet.class);
 
 	private static final PortalCache<String, byte[][]> _bytesArrayPortalCache =
-		SingleVMPoolUtil.getPortalCache(ComboServlet.class.getName());
+		SingleVMPoolUtil.getDynamicPortalCache(ComboServlet.class.getName());
 	private static final PortalCache<String, FileContentBag>
-		_fileContentBagPortalCache = SingleVMPoolUtil.getPortalCache(
+		_fileContentBagPortalCache = SingleVMPoolUtil.getDynamicPortalCache(
 			FileContentBag.class.getName());
 
 	private final Set<String> _protectedParameters = SetUtil.fromArray(
