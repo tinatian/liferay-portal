c4cf952af5674d3bd0057d9fe5f049ee8103ca99
Tina Tian <tina.tian@liferay.com>
2016-09-15 10:31:00 -0700

    LPS-68184 Rewrite the logic of removePathParameters to gain more performance, no logic changes.

--- portal-impl/src/com/liferay/portal/util/HttpImpl.java
+++ portal-impl/src/com/liferay/portal/util/HttpImpl.java
@@ -1042,37 +1042,38 @@ public class HttpImpl implements Http {
 			return uri;
 		}
 
-		int pos = uri.indexOf(CharPool.SEMICOLON);
+		int pos = 0;
 
-		if (pos == -1) {
-			return uri;
-		}
+		do {
+			int start = uri.indexOf(CharPool.SEMICOLON, pos);
 
-		String[] uriParts = StringUtil.split(uri.substring(1), CharPool.SLASH);
-
-		StringBundler sb = new StringBundler(uriParts.length * 2);
-
-		for (String uriPart : uriParts) {
-			pos = uriPart.indexOf(CharPool.SEMICOLON);
-
-			if (pos == -1) {
-				sb.append(StringPool.SLASH);
-				sb.append(uriPart);
+			if (start == -1) {
+				return uri;
 			}
-			else if (pos == 0) {
-				continue;
+			else if (start == 0) {
+				throw new IllegalArgumentException(
+					"Unable to handle uri :" + uri);
 			}
-			else {
-				sb.append(StringPool.SLASH);
-				sb.append(uriPart.substring(0, pos));
+
+			int end = uri.indexOf(CharPool.SLASH, start);
+
+			if (end == -1) {
+				if ((start != 1) && (uri.charAt(start - 1) == CharPool.SLASH)) {
+					start = start - 1;
+				}
+
+				return uri.substring(0, start);
 			}
-		}
 
-		if (sb.length() == 0) {
-			return StringPool.SLASH;
-		}
+			if (uri.charAt(start - 1) == CharPool.SLASH) {
+				start = start - 1;
+			}
 
-		return sb.toString();
+			uri = uri.substring(0, start) + uri.substring(end);
+
+			pos = start + 1;
+		}
+		while (true);
 	}
 
 	@Override
