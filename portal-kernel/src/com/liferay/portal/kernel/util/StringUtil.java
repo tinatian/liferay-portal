90a3b0fd33564296ebbc51c5018dc7953dc26fc5
Tina Tian <tina.tian@liferay.com>
2017-01-06 16:40:01 -0800

    LPS-69980 Fix StringUtil

--- portal-kernel/src/com/liferay/portal/kernel/util/StringUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/StringUtil.java
@@ -487,6 +487,14 @@ public class StringUtil {
 	 *         <code>false</code> otherwise
 	 */
 	public static boolean equals(String s1, String s2) {
+		if (s1 == s2) {
+			return true;
+		}
+
+		if ((s1 == null) || (s2 == null)) {
+			return false;
+		}
+
 		return s1.equals(s2);
 	}
 
