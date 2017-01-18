bf044af6d946d4be066b3aca769c2e7f05e84145
Tina Tian <tina.tian@liferay.com>
2017-01-18 14:19:42 -0800

    LPS-x Add UpgradeProcessCompletionMarker

--- modules/apps/foundation/portal/portal-upgrade/src/main/java/com/liferay/portal/upgrade/internal/release/ReleaseManagerOSGiCommands.java
+++ modules/apps/foundation/portal/portal-upgrade/src/main/java/com/liferay/portal/upgrade/internal/release/ReleaseManagerOSGiCommands.java
@@ -28,6 +28,7 @@ import com.liferay.portal.kernel.dao.db.DBProcessContext;
 import com.liferay.portal.kernel.model.Release;
 import com.liferay.portal.kernel.service.ReleaseLocalService;
 import com.liferay.portal.kernel.upgrade.UpgradeStep;
+import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.output.stream.container.OutputStreamContainer;
@@ -36,11 +37,13 @@ import com.liferay.portal.output.stream.container.OutputStreamContainerFactoryTr
 import com.liferay.portal.upgrade.internal.configuration.ReleaseManagerConfiguration;
 import com.liferay.portal.upgrade.internal.graph.ReleaseGraphManager;
 import com.liferay.portal.upgrade.registry.UpgradeInfo;
+import com.liferay.portal.upgrade.release.UpgradeProcessCompletionMarker;
 
 import java.io.IOException;
 import java.io.OutputStream;
 
 import java.util.Collections;
+import java.util.Dictionary;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -50,6 +53,7 @@ import org.apache.felix.utils.log.Logger;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
@@ -195,6 +199,8 @@ public class ReleaseManagerOSGiCommands {
 	protected void activate(
 		final BundleContext bundleContext, Map<String, Object> properties) {
 
+		_bundleContext = bundleContext;
+
 		_logger = new Logger(bundleContext);
 
 		DB db = DBManagerUtil.getDB();
@@ -226,6 +232,19 @@ public class ReleaseManagerOSGiCommands {
 	@Deactivate
 	protected void deactivate() {
 		_serviceTrackerMap.close();
+
+		for (Map.Entry
+				<String, ServiceRegistration<UpgradeProcessCompletionMarker>>
+					serviceRegistrationEntry :
+						_serviceRegistrations.entrySet()) {
+
+			ServiceRegistration<UpgradeProcessCompletionMarker>
+				serviceRegistration = serviceRegistrationEntry.getValue();
+
+			serviceRegistration.unregister();
+		}
+
+		_serviceRegistrations = null;
 	}
 
 	protected void doExecute(
@@ -369,11 +388,14 @@ public class ReleaseManagerOSGiCommands {
 
 	private static Logger _logger;
 
+	private BundleContext _bundleContext;
 	private OutputStreamContainerFactoryTracker
 		_outputStreamContainerFactoryTracker;
 	private ReleaseLocalService _releaseLocalService;
 	private ReleaseManagerConfiguration _releaseManagerConfiguration;
 	private ReleasePublisher _releasePublisher;
+	private Map<String, ServiceRegistration<UpgradeProcessCompletionMarker>>
+		_serviceRegistrations;
 	private ServiceTrackerMap<String, List<UpgradeInfo>> _serviceTrackerMap;
 
 	private static class UpgradeServiceTrackerCustomizer
@@ -490,6 +512,17 @@ public class ReleaseManagerOSGiCommands {
 					throw new RuntimeException(e);
 				}
 			}
+
+			Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+			properties.put("upgrade.bundle.symbolic.name", _bundleSymbolicName);
+
+			ServiceRegistration<UpgradeProcessCompletionMarker>
+				serviceRegistration = _bundleContext.registerService(
+					UpgradeProcessCompletionMarker.class,
+					new UpgradeProcessCompletionMarker() {}, properties);
+
+			_serviceRegistrations.put(_bundleSymbolicName, serviceRegistration);
 
 			CacheRegistryUtil.clear();
 		}
