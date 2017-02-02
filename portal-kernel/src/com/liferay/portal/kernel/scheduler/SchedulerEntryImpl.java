5461091448626959732584f69d154a6b6374cf85
Tina Tian <tina.tian@liferay.com>
2017-02-01 16:09:56 -0800

    LPS-69996 Modify SchedulerEntryImpl to avoid using set*** methods, set*** methods are only used for backward compatibility

--- portal-kernel/src/com/liferay/portal/kernel/scheduler/SchedulerEntryImpl.java
+++ portal-kernel/src/com/liferay/portal/kernel/scheduler/SchedulerEntryImpl.java
@@ -22,6 +22,26 @@ import com.liferay.portal.kernel.util.StringPool;
  */
 public class SchedulerEntryImpl implements SchedulerEntry {
 
+	/**
+	 * @deprecated As of 7.0.0
+	 */
+	@Deprecated
+	public SchedulerEntryImpl() {
+		this(StringPool.BLANK, null, StringPool.BLANK);
+	}
+
+	public SchedulerEntryImpl(String eventListenerClass, Trigger trigger) {
+		this(eventListenerClass, trigger, StringPool.BLANK);
+	}
+
+	public SchedulerEntryImpl(
+		String eventListenerClass, Trigger trigger, String description) {
+
+		_eventListenerClass = eventListenerClass;
+		_trigger = trigger;
+		_description = description;
+	}
+
 	@Override
 	public String getDescription() {
 		return _description;
@@ -37,14 +57,26 @@ public class SchedulerEntryImpl implements SchedulerEntry {
 		return _trigger;
 	}
 
+	/**
+	 * @deprecated As of 7.0.0
+	 */
+	@Deprecated
 	public void setDescription(String description) {
 		_description = description;
 	}
 
+	/**
+	 * @deprecated As of 7.0.0
+	 */
+	@Deprecated
 	public void setEventListenerClass(String eventListenerClass) {
 		_eventListenerClass = eventListenerClass;
 	}
 
+	/**
+	 * @deprecated As of 7.0.0
+	 */
+	@Deprecated
 	public void setTrigger(Trigger trigger) {
 		_trigger = trigger;
 	}
@@ -62,8 +94,8 @@ public class SchedulerEntryImpl implements SchedulerEntry {
 		return sb.toString();
 	}
 
-	private String _description = StringPool.BLANK;
-	private String _eventListenerClass = StringPool.BLANK;
+	private String _description;
+	private String _eventListenerClass;
 	private Trigger _trigger;
 
 }
