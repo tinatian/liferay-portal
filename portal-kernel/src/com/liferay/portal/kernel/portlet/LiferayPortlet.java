b38b1892aa0daf756f7f51466c42362246b75551
Tina Tian <tina.tian@liferay.com>
2017-02-08 15:36:06 -0800

    LPS-X Reuse value instead of calculating twice

--- portal-kernel/src/com/liferay/portal/kernel/portlet/LiferayPortlet.java
+++ portal-kernel/src/com/liferay/portal/kernel/portlet/LiferayPortlet.java
@@ -101,7 +101,7 @@ public class LiferayPortlet extends GenericPortlet {
 			boolean emptySessionMessages = isEmptySessionMessages(
 				actionRequest);
 
-			if (isAddSuccessMessage(actionRequest)) {
+			if (isAddSuccessMessage(actionRequest, emptySessionMessages)) {
 				addSuccessMessage(actionRequest, actionResponse);
 			}
 
@@ -499,7 +499,19 @@ public class LiferayPortlet extends GenericPortlet {
 			Arrays.asList(StringUtil.split(getInitParameter("valid-paths"))));
 	}
 
+	/**
+	 * @deprecated As of 7.0.0, replaced by {@link #isAddSuccessMessage(
+	 * ActionRequest, boolean)}
+	 */
+	@Deprecated
 	protected boolean isAddSuccessMessage(ActionRequest actionRequest) {
+		return isAddSuccessMessage(
+			actionRequest, isEmptySessionMessages(actionRequest));
+	}
+
+	protected boolean isAddSuccessMessage(
+		ActionRequest actionRequest, boolean emptySessionMessages) {
+
 		if (!addProcessActionSuccessMessage) {
 			return false;
 		}
@@ -513,23 +525,7 @@ public class LiferayPortlet extends GenericPortlet {
 			return false;
 		}
 
-		if (SessionMessages.isEmpty(actionRequest)) {
-			return true;
-		}
-
-		int sessionMessagesSize = SessionMessages.size(actionRequest);
-
-		for (String suffix : _IGNORED_SESSION_MESSAGE_SUFFIXES) {
-			if (SessionMessages.contains(actionRequest, portletId + suffix)) {
-				sessionMessagesSize--;
-			}
-		}
-
-		if (sessionMessagesSize == 0) {
-			return true;
-		}
-
-		return false;
+		return emptySessionMessages;
 	}
 
 	protected boolean isAlwaysSendRedirect() {
