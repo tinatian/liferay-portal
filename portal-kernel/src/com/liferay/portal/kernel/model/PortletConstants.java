9ccbc080b26a318779c6a82fc98dc4f668f4c1d9
Tina Tian <tina.tian@liferay.com>
2017-05-10 13:39:10 -0700

    LPS-72505 Copy logic of hasInstanceId() from PortletInstance to PortletConstants, prepare to deprecate PortletInstance

--- portal-kernel/src/com/liferay/portal/kernel/model/PortletConstants.java
+++ portal-kernel/src/com/liferay/portal/kernel/model/PortletConstants.java
@@ -222,10 +222,7 @@ public class PortletConstants {
 	 *         <code>false</code> otherwise
 	 */
 	public static boolean hasInstanceId(String portletId) {
-		PortletInstance portletInstance =
-			PortletInstance.fromPortletInstanceKey(portletId);
-
-		return portletInstance.hasInstanceId();
+		return Validator.isNotNull(getInstanceId(portletId));
 	}
 
 	/**
