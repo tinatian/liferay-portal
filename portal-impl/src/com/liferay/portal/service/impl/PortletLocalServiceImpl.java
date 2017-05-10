964c833d5301a9f1efdc13e2f58c71edbcde7c21
Tina Tian <tina.tian@liferay.com>
2017-05-10 10:42:49 -0700

    LPS-x Apply usage

--- portal-impl/src/com/liferay/portal/service/impl/PortletLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/PortletLocalServiceImpl.java
@@ -35,7 +35,6 @@ import com.liferay.portal.kernel.model.PortletCategory;
 import com.liferay.portal.kernel.model.PortletConstants;
 import com.liferay.portal.kernel.model.PortletFilter;
 import com.liferay.portal.kernel.model.PortletInfo;
-import com.liferay.portal.kernel.model.PortletInstance;
 import com.liferay.portal.kernel.model.PortletPreferences;
 import com.liferay.portal.kernel.model.PortletURLListener;
 import com.liferay.portal.kernel.model.PublicRenderParameter;
@@ -2073,15 +2072,13 @@ public class PortletLocalServiceImpl extends PortletLocalServiceBaseImpl {
 
 		portletId = PortalUtil.getJsSafePortletId(portletId);
 
-		if (portletId.length() >
-				PortletInstance.PORTLET_INSTANCE_KEY_MAX_LENGTH) {
+		if (portletId.length() > PortalUtil.getMaxPortletIdLength()) {
 
 			// LPS-32878
 
 			throw new PortletIdException(
 				"Portlet ID " + portletId + " has more than " +
-					PortletInstance.PORTLET_INSTANCE_KEY_MAX_LENGTH +
-						" characters");
+					PortalUtil.getMaxPortletIdLength() + " characters");
 		}
 
 		if (_log.isDebugEnabled()) {
