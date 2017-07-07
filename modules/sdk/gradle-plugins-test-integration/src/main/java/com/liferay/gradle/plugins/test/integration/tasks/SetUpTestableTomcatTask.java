be13401d17e86aef284c1aaf566609ff1edadfb0
tina.tian <tina.tian@liferay.com>
2017-07-07 16:29:17 -0700

    LPS-73525 SF, group changes to setenv to a new method like what we did in ant script

--- modules/sdk/gradle-plugins-test-integration/src/main/java/com/liferay/gradle/plugins/test/integration/tasks/SetUpTestableTomcatTask.java
+++ modules/sdk/gradle-plugins-test-integration/src/main/java/com/liferay/gradle/plugins/test/integration/tasks/SetUpTestableTomcatTask.java
@@ -212,12 +212,10 @@ public class SetUpTestableTomcatTask
 
 	@TaskAction
 	public void setUpTestableTomcat() throws Exception {
-		_setUpAspectJ();
-		_setUpJmx();
-		_setUpJpda();
 		_setUpLogging();
 		_setUpManager();
 		_setUpOsgiModules();
+		_setUpSetEnv();
 	}
 
 	public void setZipUrl(Object zipUrl) {
@@ -518,6 +516,12 @@ public class SetUpTestableTomcatTask
 				}
 
 			});
+	}
+
+	private void _setUpSetEnv() throws IOException {
+		_setUpAspectJ();
+		_setUpJmx();
+		_setUpJpda();
 	}
 
 	private static final String[] _TOMCAT_USERS_ROLE_NAMES = {
