f655279b1dd2da2c3b306cf7310f54adcec525e3
tina.tian <tina.tian@liferay.com>
2017-07-11 14:36:12 -0700

    LPS-73353 Add jacoco support for tomcat setup (gradle), jacoco agent must be the first java agent

--- modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/internal/TestIntegrationDefaultsPlugin.java
+++ modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/internal/TestIntegrationDefaultsPlugin.java
@@ -93,6 +93,9 @@ public class TestIntegrationDefaultsPlugin
 			GradleUtil.getProperty(
 				project, "aspectj.configuration", (String)null));
 
+		setUpTestableTomcatTask.setJaCoCoAgent(
+			GradleUtil.getProperty(project, "jacoco.agent", (String)null));
+
 		setUpTestableTomcatTask.setZipUrl(
 			new Callable<String>() {
 
