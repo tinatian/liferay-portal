2fcb98bfa0ca3dbb6d9c813b6c4198aef674b0ef
tina.tian <tina.tian@liferay.com>
2017-09-29 15:53:57 -0700

    LPS-74685 Should only pass in render parameters rather than all parameters

--- portal-impl/src/com/liferay/portlet/EventResponseImpl.java
+++ portal-impl/src/com/liferay/portlet/EventResponseImpl.java
@@ -16,6 +16,8 @@ package com.liferay.portlet;
 
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.util.PortalUtil;
 
 import javax.portlet.EventRequest;
 import javax.portlet.EventResponse;
@@ -42,7 +44,10 @@ public class EventResponseImpl
 			throw new IllegalArgumentException();
 		}
 
-		setRenderParameters(eventRequest.getParameterMap());
+		LiferayPortletRequest liferayPortletRequest =
+			PortalUtil.getLiferayPortletRequest(eventRequest);
+
+		setRenderParameters(liferayPortletRequest.getRenderParameters());
 	}
 
 	/**
