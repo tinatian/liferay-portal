5005eaa2e3cb043e29035192eab3b1be46b9626a
Lance Ji <zheyi.ji@liferay.com>
2017-10-17 18:10:47 -0700

    LPS-75199 Test internationalization settings of modules

--- modules/apps/foundation/configuration-admin/configuration-admin-test/src/testIntegration/java/com/liferay/configuration/admin/test/ConfigurationLocalizationTest.java
+++ modules/apps/foundation/configuration-admin/configuration-admin-test/src/testIntegration/java/com/liferay/configuration/admin/test/ConfigurationLocalizationTest.java
@@ -15,14 +15,38 @@
 package com.liferay.configuration.admin.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.configuration.metatype.definitions.ExtendedAttributeDefinition;
+import com.liferay.portal.configuration.metatype.definitions.ExtendedMetaTypeInformation;
 import com.liferay.portal.configuration.metatype.definitions.ExtendedMetaTypeService;
+import com.liferay.portal.configuration.metatype.definitions.ExtendedObjectClassDefinition;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.ResourceBundleLoader;
+import com.liferay.portal.kernel.util.ResourceBundleLoaderUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.service.metatype.ObjectClassDefinition;
 
 /**
  * @author Lance Ji
@@ -34,6 +58,118 @@ public class ConfigurationLocalizationTest {
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
 		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testBundleLocalizationTest() {
+		Bundle currentBundle = FrameworkUtil.getBundle(
+			ConfigurationLocalizationTest.class);
+
+		BundleContext bundleContext = currentBundle.getBundleContext();
+
+		Map<String, List<String>> errorMap = new HashMap<>();
+
+		for (Bundle bundle : bundleContext.getBundles()) {
+			List<String> errorMessageList = new ArrayList<>();
+
+			String currentBundleSymbolicName = bundle.getSymbolicName();
+
+			ExtendedMetaTypeInformation extendedMetaTypeInformation =
+				_extendedMetaTypeService.getMetaTypeInformation(bundle);
+
+			List<String> pids = new ArrayList<>();
+
+			Collections.addAll(
+				pids, extendedMetaTypeInformation.getFactoryPids());
+
+			Collections.addAll(pids, extendedMetaTypeInformation.getPids());
+
+			if (pids.isEmpty()) {
+				continue;
+			}
+
+			ResourceBundleLoader resourceBundleLoader =
+				ResourceBundleLoaderUtil.
+					getResourceBundleLoaderByBundleSymbolicName(
+						currentBundleSymbolicName);
+
+			if (resourceBundleLoader == null) {
+				errorMessageList.add("No resource bundle");
+				errorMap.put(currentBundleSymbolicName, errorMessageList);
+				continue;
+			}
+
+			ResourceBundle koResourceBundle =
+				resourceBundleLoader.loadResourceBundle(Locale.KOREA);
+
+			ResourceBundle resourceBundle =
+				resourceBundleLoader.loadResourceBundle(Locale.getDefault());
+
+			if (Objects.equals(resourceBundle, koResourceBundle)) {
+				errorMessageList.add("Missing generated resource files");
+			}
+
+			for (String pid : pids) {
+				ExtendedObjectClassDefinition extendedObjectClassDefinition =
+					extendedMetaTypeInformation.getObjectClassDefinition(
+						pid, Locale.getDefault().getLanguage());
+
+				String objectClassDefinitionName = ResourceBundleUtil.getString(
+					resourceBundle, extendedObjectClassDefinition.getName());
+
+				if (objectClassDefinitionName == null) {
+					errorMessageList.add(
+						"ObjectClassDefinition name value is empty in " +
+							"configuration " + pid);
+				}
+
+				ExtendedAttributeDefinition[] extendedAttributeDefinitions =
+					extendedObjectClassDefinition.getAttributeDefinitions(
+						ObjectClassDefinition.ALL);
+
+				for (ExtendedAttributeDefinition extendedAttributeDefinition :
+						extendedAttributeDefinitions) {
+
+					String attributeDefinitionName =
+						extendedAttributeDefinition.getName();
+
+					String attributeDefinitionString =
+						ResourceBundleUtil.getString(
+							resourceBundle, attributeDefinitionName);
+
+					if (attributeDefinitionString == null) {
+						errorMessageList.add(
+							"AttributeDefinition with name: \"" +
+								attributeDefinitionName + "\" has no value " +
+									"in language.properties file, " +
+										"configurationPid: " + pid);
+					}
+				}
+			}
+
+			if (ListUtil.isNotEmpty(errorMessageList)) {
+				errorMap.put(currentBundleSymbolicName, errorMessageList);
+			}
+		}
+
+		if (MapUtil.isNotEmpty(errorMap)) {
+			Assert.fail(_printErrorMessage(errorMap));
+		}
+	}
+
+	private String _printErrorMessage(Map<String, List<String>> errorMap) {
+		String errorMessage = "";
+
+		for (String key : errorMap.keySet()) {
+			errorMessage += "\nBundle with symbolic name \"" + key + "\":\n";
+			List<String> messageList = errorMap.get(key);
+
+			for (String message : messageList) {
+				errorMessage += message + "\n";
+			}
+		}
+
+		return errorMessage;
+	}
 
 	@Inject
 	private ExtendedMetaTypeService _extendedMetaTypeService;
