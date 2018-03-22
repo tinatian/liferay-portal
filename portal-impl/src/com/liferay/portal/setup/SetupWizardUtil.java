e29aa6637dd09cded0abfc1ad076eb3ef57b8b1c
Lance Ji <zheyi.ji@liferay.com>
2018-03-22 09:49:48 -0700

    LPS-77684 Write properties with empty value to setup property file

--- portal-impl/src/com/liferay/portal/setup/SetupWizardUtil.java
+++ portal-impl/src/com/liferay/portal/setup/SetupWizardUtil.java
@@ -44,6 +44,8 @@ import java.io.IOException;
 import java.sql.Connection;
 
 import java.util.Locale;
+import java.util.Map;
+import java.util.TreeMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -177,6 +179,26 @@ public class SetupWizardUtil {
 		name = _PROPERTIES_PREFIX.concat(name).concat(StringPool.DOUBLE_DASH);
 
 		return ParamUtil.getString(request, name, defaultValue);
+	}
+
+	private static String _getUnicodePropertiesStringWithEmptyValue(
+		UnicodeProperties unicodeProperties) {
+
+		Map<String, String> treeMap = new TreeMap<>(unicodeProperties);
+
+		for (Map.Entry<String, String> entry : treeMap.entrySet()) {
+			String value = entry.getValue();
+
+			if (Validator.isNull(value)) {
+				unicodeProperties.setProperty(
+					entry.getKey(), StringPool.IS_NULL);
+			}
+		}
+
+		String encodedPropertiesString = unicodeProperties.toString();
+
+		return encodedPropertiesString.replace(
+			StringPool.IS_NULL, StringPool.BLANK);
 	}
 
 	private static boolean _isDatabaseConfigured(
@@ -358,7 +380,7 @@ public class SetupWizardUtil {
 		try {
 			FileUtil.write(
 				PropsValues.LIFERAY_HOME, PROPERTIES_FILE_NAME,
-				unicodeProperties.toString());
+				_getUnicodePropertiesStringWithEmptyValue(unicodeProperties));
 
 			if (FileUtil.exists(
 					PropsValues.LIFERAY_HOME + StringPool.SLASH +
