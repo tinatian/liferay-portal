376041744f27aced765a6c5445be7ec826598e5d
tina.tian <tina.tian@liferay.com>
2018-04-26 09:33:09 -0700

    LPS-79629 Rename variables to make it more accurate, the context name get from ClassLoaderPool may not be a servlet context name.

--- portal-kernel/src/com/liferay/portal/kernel/util/SubscriptionSender.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/SubscriptionSender.java
@@ -1010,10 +1010,10 @@ public class SubscriptionSender implements Serializable {
 
 		objectInputStream.defaultReadObject();
 
-		String servletContextName = objectInputStream.readUTF();
+		String contextName = objectInputStream.readUTF();
 
-		if (!servletContextName.isEmpty()) {
-			_classLoader = ClassLoaderPool.getClassLoader(servletContextName);
+		if (!contextName.isEmpty()) {
+			_classLoader = ClassLoaderPool.getClassLoader(contextName);
 		}
 	}
 
@@ -1022,13 +1022,13 @@ public class SubscriptionSender implements Serializable {
 
 		objectOutputStream.defaultWriteObject();
 
-		String servletContextName = StringPool.BLANK;
+		String contextName = StringPool.BLANK;
 
 		if (_classLoader != null) {
-			servletContextName = ClassLoaderPool.getContextName(_classLoader);
+			contextName = ClassLoaderPool.getContextName(_classLoader);
 		}
 
-		objectOutputStream.writeUTF(servletContextName);
+		objectOutputStream.writeUTF(contextName);
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
