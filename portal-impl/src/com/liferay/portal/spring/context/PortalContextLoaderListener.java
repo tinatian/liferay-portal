6b7fc4207f840c6acd317727e730eda5eb93474e
tina.tian <tina.tian@liferay.com>
2018-11-07 17:25:52 -0800

    LPS-84130 Unregister portal class loader when destroying

--- portal-impl/src/com/liferay/portal/spring/context/PortalContextLoaderListener.java
+++ portal-impl/src/com/liferay/portal/spring/context/PortalContextLoaderListener.java
@@ -174,6 +174,9 @@ public class PortalContextLoaderListener extends ContextLoaderListener {
 		ModuleFrameworkUtilAdapter.unregisterContext(_arrayApplicationContext);
 
 		_arrayApplicationContext.close();
+
+		ClassLoaderPool.unregister(_portalServletContextName);
+		ServletContextClassLoaderPool.unregister(_portalServletContextName);
 	}
 
 	@Override
