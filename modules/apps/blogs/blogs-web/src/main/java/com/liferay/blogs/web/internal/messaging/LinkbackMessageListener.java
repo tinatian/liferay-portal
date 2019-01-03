16bd870b95508f86eb400eaae1633cb747fd08ca
hai.yu <8219754@liferay.com>
2019-01-03 16:47:16 +0800

    LPS-88710 Invert service dependency to avoid calling SchedulerEngineHelper.register(): declare the listeners as SchedulerEventMessageListener services

--- modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/messaging/LinkbackMessageListener.java
+++ modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/messaging/LinkbackMessageListener.java
@@ -21,19 +21,18 @@ import com.liferay.portal.kernel.messaging.BaseMessageListener;
 import com.liferay.portal.kernel.messaging.DestinationNames;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
-import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
 import com.liferay.portal.kernel.scheduler.SchedulerEntry;
 import com.liferay.portal.kernel.scheduler.SchedulerEntryImpl;
 import com.liferay.portal.kernel.scheduler.TimeUnit;
 import com.liferay.portal.kernel.scheduler.Trigger;
 import com.liferay.portal.kernel.scheduler.TriggerFactory;
+import com.liferay.portal.kernel.scheduler.messaging.SchedulerEventMessageListener;
 import com.liferay.portal.linkback.LinkbackProducerUtil;
 
 import java.util.Map;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
 
@@ -43,9 +42,19 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(
 	configurationPid = "com.liferay.blogs.configuration.BlogsConfiguration",
-	immediate = true, service = LinkbackMessageListener.class
+	immediate = true,
+	property = "destination.name=" + DestinationNames.SCHEDULER_DISPATCH,
+	service = {
+		LinkbackMessageListener.class, SchedulerEventMessageListener.class
+	}
 )
-public class LinkbackMessageListener extends BaseMessageListener {
+public class LinkbackMessageListener
+	extends BaseMessageListener implements SchedulerEventMessageListener {
+
+	@Override
+	public SchedulerEntry getSchedulerEntry() {
+		return _schedulerEntry;
+	}
 
 	@Activate
 	@Modified
@@ -61,16 +70,7 @@ public class LinkbackMessageListener extends BaseMessageListener {
 			className, className, null, null,
 			_blogsConfiguration.linkbackJobInterval(), TimeUnit.MINUTE);
 
-		SchedulerEntry schedulerEntry = new SchedulerEntryImpl(
-			className, trigger);
-
-		_schedulerEngineHelper.register(
-			this, schedulerEntry, DestinationNames.SCHEDULER_DISPATCH);
-	}
-
-	@Deactivate
-	protected void deactivate() {
-		_schedulerEngineHelper.unregister(this);
+		_schedulerEntry = new SchedulerEntryImpl(className, trigger);
 	}
 
 	@Override
@@ -85,19 +85,12 @@ public class LinkbackMessageListener extends BaseMessageListener {
 		ModuleServiceLifecycle moduleServiceLifecycle) {
 	}
 
-	@Reference(unbind = "-")
-	protected void setSchedulerEngineHelper(
-		SchedulerEngineHelper schedulerEngineHelper) {
-
-		_schedulerEngineHelper = schedulerEngineHelper;
-	}
-
 	private volatile BlogsConfiguration _blogsConfiguration;
 
 	@Reference
 	private LinkbackConsumer _linkbackConsumer;
 
-	private SchedulerEngineHelper _schedulerEngineHelper;
+	private SchedulerEntry _schedulerEntry;
 
 	@Reference
 	private TriggerFactory _triggerFactory;
--- modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/messaging/AsahFaroBackendMessageListener.java
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/messaging/AsahFaroBackendMessageListener.java
@@ -19,12 +19,12 @@ import com.liferay.portal.kernel.messaging.BaseMessageListener;
 import com.liferay.portal.kernel.messaging.DestinationNames;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
-import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
 import com.liferay.portal.kernel.scheduler.SchedulerEntry;
 import com.liferay.portal.kernel.scheduler.SchedulerEntryImpl;
 import com.liferay.portal.kernel.scheduler.TimeUnit;
 import com.liferay.portal.kernel.scheduler.Trigger;
 import com.liferay.portal.kernel.scheduler.TriggerFactory;
+import com.liferay.portal.kernel.scheduler.messaging.SchedulerEventMessageListener;
 import com.liferay.segments.internal.configuration.SegmentsServiceConfiguration;
 
 import java.util.Map;
@@ -32,7 +32,6 @@ import java.util.Map;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
-import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
 
@@ -42,9 +41,19 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	configurationPid = "com.liferay.journal.configuration.SegmentsServiceConfiguration",
 	configurationPolicy = ConfigurationPolicy.OPTIONAL, immediate = true,
-	service = AsahFaroBackendMessageListener.class
+	property = "destination.name=" + DestinationNames.SCHEDULER_DISPATCH,
+	service = {
+		AsahFaroBackendMessageListener.class,
+		SchedulerEventMessageListener.class
+	}
 )
-public class AsahFaroBackendMessageListener extends BaseMessageListener {
+public class AsahFaroBackendMessageListener
+	extends BaseMessageListener implements SchedulerEventMessageListener {
+
+	@Override
+	public SchedulerEntry getSchedulerEntry() {
+		return _schedulerEntry;
+	}
 
 	@Activate
 	@Modified
@@ -59,16 +68,7 @@ public class AsahFaroBackendMessageListener extends BaseMessageListener {
 			clazz.getName(), clazz.getName(), null, null,
 			segmentsServiceConfiguration.checkInterval(), TimeUnit.MINUTE);
 
-		SchedulerEntry schedulerEntry = new SchedulerEntryImpl(
-			clazz.getName(), trigger);
-
-		_schedulerEngineHelper.register(
-			this, schedulerEntry, DestinationNames.SCHEDULER_DISPATCH);
-	}
-
-	@Deactivate
-	protected void deactivate() {
-		_schedulerEngineHelper.unregister(this);
+		_schedulerEntry = new SchedulerEntryImpl(clazz.getName(), trigger);
 	}
 
 	@Override
@@ -90,16 +90,9 @@ public class AsahFaroBackendMessageListener extends BaseMessageListener {
 		ModuleServiceLifecycle moduleServiceLifecycle) {
 	}
 
-	@Reference(unbind = "-")
-	protected void setSchedulerEngineHelper(
-		SchedulerEngineHelper schedulerEngineHelper) {
-
-		_schedulerEngineHelper = schedulerEngineHelper;
-	}
-
 	private AsahFaroBackendIndividualSegmentsCheckerUtil
 		_asahFaroBackendIndividualSegmentsCheckerUtil;
-	private SchedulerEngineHelper _schedulerEngineHelper;
+	private SchedulerEntry _schedulerEntry;
 
 	@Reference
 	private TriggerFactory _triggerFactory;
