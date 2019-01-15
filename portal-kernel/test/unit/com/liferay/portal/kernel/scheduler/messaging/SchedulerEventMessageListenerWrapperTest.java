0d5c9f4a1d4a9ed731c4440899b0079074120eee
tina.tian <tina.tian@liferay.com>
2019-01-15 14:08:57 -0800

    LPS-89033 Fix unit test

--- portal-kernel/test/unit/com/liferay/portal/kernel/scheduler/messaging/SchedulerEventMessageListenerWrapperTest.java
+++ portal-kernel/test/unit/com/liferay/portal/kernel/scheduler/messaging/SchedulerEventMessageListenerWrapperTest.java
@@ -15,23 +15,42 @@
 package com.liferay.portal.kernel.scheduler.messaging;
 
 import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageBus;
 import com.liferay.portal.kernel.messaging.MessageListener;
+import com.liferay.portal.kernel.test.rule.NewEnv;
+import com.liferay.portal.kernel.test.rule.NewEnvTestRule;
+import com.liferay.portal.kernel.test.util.PropsTestUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.ProxyUtil;
 import com.liferay.registry.BasicRegistryImpl;
+import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 
 /**
  * @author Tina Tian
  */
+@NewEnv(type = NewEnv.Type.CLASSLOADER)
 public class SchedulerEventMessageListenerWrapperTest {
+
+	@ClassRule
+	@Rule
+	public static final NewEnvTestRule newEnvTestRule = NewEnvTestRule.INSTANCE;
 
 	@Before
 	public void setUp() {
@@ -39,7 +58,10 @@ public class SchedulerEventMessageListenerWrapperTest {
 	}
 
 	@Test
-	public void testConcurrentInvocation() throws Exception {
+	public void testConcurrentReceiveWithoutTimeout() throws Exception {
+		PropsTestUtil.setProps(
+			PropsKeys.SCHEDULER_EVENT_MESSAGE_LISTENER_LOCK_TIMEOUT, "0");
+
 		SchedulerEventMessageListenerWrapper
 			schedulerEventMessageListenerWrapper =
 				new SchedulerEventMessageListenerWrapper();
@@ -51,33 +73,146 @@ public class SchedulerEventMessageListenerWrapperTest {
 
 		Message message1 = new Message();
 
+		FutureTask<Void> futureTask1 = _startThread(
+			schedulerEventMessageListenerWrapper, "Thread1", message1);
+
+		testMessageListener.waitUntilBlock();
+
+		Message message2 = new Message();
+
+		FutureTask<Void> futureTask2 = _startThread(
+			schedulerEventMessageListenerWrapper, "Thread2", message2);
+
+		try {
+			futureTask2.get(1000, TimeUnit.MICROSECONDS);
+
+			Assert.fail();
+		}
+		catch (Exception e) {
+			Assert.assertTrue(e instanceof TimeoutException);
+		}
+
+		testMessageListener.unblock();
+
+		futureTask1.get();
+		futureTask2.get();
+
+		Assert.assertSame(
+			"Message is not processed", message1, message1.getResponse());
+		Assert.assertSame(
+			"Message is not processed", message2, message2.getResponse());
+	}
+
+	@Test
+	public void testConcurrentReceiveWithTimeout() throws Exception {
+		PropsTestUtil.setProps(
+			PropsKeys.SCHEDULER_EVENT_MESSAGE_LISTENER_LOCK_TIMEOUT, "1");
+
+		SchedulerEventMessageListenerWrapper
+			schedulerEventMessageListenerWrapper =
+				new SchedulerEventMessageListenerWrapper();
+
+		TestMessageListener testMessageListener = new TestMessageListener();
+
+		schedulerEventMessageListenerWrapper.setMessageListener(
+			testMessageListener);
+
+		Registry registry = RegistryUtil.getRegistry();
+
+		TestMessageBusInvocationHandler testMessageBusInvocationHandler =
+			new TestMessageBusInvocationHandler(
+				schedulerEventMessageListenerWrapper);
+
+		registry.registerService(
+			MessageBus.class,
+			(MessageBus)ProxyUtil.newProxyInstance(
+				MessageBus.class.getClassLoader(),
+				new Class<?>[] {MessageBus.class},
+				testMessageBusInvocationHandler));
+
+		Message message1 = new Message();
+
+		FutureTask<Void> futureTask1 = _startThread(
+			schedulerEventMessageListenerWrapper, "Thread1", message1);
+
+		testMessageListener.waitUntilBlock();
+
+		Message message2 = new Message();
+
+		FutureTask<Void> futureTask2 = _startThread(
+			schedulerEventMessageListenerWrapper, "Thread2", message2);
+
+		futureTask2.get();
+
+		testMessageListener.unblock();
+
+		FutureTask<Void> resendFutureTask =
+			testMessageBusInvocationHandler.getFutureTask();
+
+		futureTask1.get();
+		resendFutureTask.get();
+
+		Assert.assertSame(
+			"Message is not processed", message1, message1.getResponse());
+		Assert.assertSame(
+			"Message is not processed", message2, message2.getResponse());
+	}
+
+	private FutureTask<Void> _startThread(
+		SchedulerEventMessageListenerWrapper
+			schedulerEventMessageListenerWrapper,
+		String threadName, Message message) {
+
 		FutureTask<Void> futureTask = new FutureTask<>(
 			() -> {
-				schedulerEventMessageListenerWrapper.receive(message1);
+				schedulerEventMessageListenerWrapper.receive(message);
 
 				return null;
 			});
 
 		Thread thread = new Thread(
 			futureTask,
-			"SchedulerEventMessageListenerWrapperTest_testConcurrent_Thread");
+			"SchedulerEventMessageListenerWrapperTest_startThread_" +
+				threadName);
 
 		thread.start();
 
-		testMessageListener.waitUntilBlock();
+		return futureTask;
+	}
 
-		Message message2 = new Message();
+	private class TestMessageBusInvocationHandler implements InvocationHandler {
 
-		schedulerEventMessageListenerWrapper.receive(message2);
+		public FutureTask<Void> getFutureTask() {
+			return _futureTask;
+		}
 
-		testMessageListener.unblock();
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args)
+			throws Throwable {
 
-		futureTask.get();
+			String methodName = method.getName();
 
-		Assert.assertSame(
-			"Message is not processed", message1, message1.getResponse());
-		Assert.assertSame(
-			"Message is not processed", message2, message2.getResponse());
+			if (methodName.equals("sendMessage")) {
+				_futureTask = _startThread(
+					_schedulerEventMessageListenerWrapper, "Resend",
+					(Message)args[1]);
+			}
+
+			return null;
+		}
+
+		private TestMessageBusInvocationHandler(
+			SchedulerEventMessageListenerWrapper
+				schedulerEventMessageListenerWrapper) {
+
+			_schedulerEventMessageListenerWrapper =
+				schedulerEventMessageListenerWrapper;
+		}
+
+		private FutureTask<Void> _futureTask;
+		private final SchedulerEventMessageListenerWrapper
+			_schedulerEventMessageListenerWrapper;
+
 	}
 
 	private class TestMessageListener implements MessageListener {
