c322b5955431ffceb92f889f52cf5f65d6b151bd
tina.tian <tina.tian@liferay.com>
2019-01-28 17:04:13 -0800

    LPS-89456 Auto generated build-upgrade-table - portal core

--- portal-impl/src/com/liferay/portal/upgrade/v7_2_x/util/LayoutBranchTable.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_2_x/util/LayoutBranchTable.java
@@ -20,26 +20,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @author	  Brian Wing Shun Chan
+ * @author	  Tina Tian
  * @generated
  */
-public class TeamTable {
+public class LayoutBranchTable {
 
-	public static final String TABLE_NAME = "Team";
+	public static final String TABLE_NAME = "LayoutBranch";
 
 	public static final Object[][] TABLE_COLUMNS = {
 		{"mvccVersion", Types.BIGINT},
-		{"uuid_", Types.VARCHAR},
-		{"teamId", Types.BIGINT},
+		{"layoutBranchId", Types.BIGINT},
+		{"groupId", Types.BIGINT},
 		{"companyId", Types.BIGINT},
 		{"userId", Types.BIGINT},
 		{"userName", Types.VARCHAR},
-		{"createDate", Types.TIMESTAMP},
-		{"modifiedDate", Types.TIMESTAMP},
-		{"groupId", Types.BIGINT},
+		{"layoutSetBranchId", Types.BIGINT},
+		{"plid", Types.BIGINT},
 		{"name", Types.VARCHAR},
 		{"description_", Types.VARCHAR},
-		{"lastPublishDate", Types.TIMESTAMP}
+		{"master", Types.BOOLEAN}
 	};
 
 	public static final Map<String, Integer> TABLE_COLUMNS_MAP = new HashMap<String, Integer>();
@@ -47,9 +46,9 @@ public class TeamTable {
 static {
 TABLE_COLUMNS_MAP.put("mvccVersion", Types.BIGINT);
 
-TABLE_COLUMNS_MAP.put("uuid_", Types.VARCHAR);
+TABLE_COLUMNS_MAP.put("layoutBranchId", Types.BIGINT);
 
-TABLE_COLUMNS_MAP.put("teamId", Types.BIGINT);
+TABLE_COLUMNS_MAP.put("groupId", Types.BIGINT);
 
 TABLE_COLUMNS_MAP.put("companyId", Types.BIGINT);
 
@@ -57,27 +56,24 @@ TABLE_COLUMNS_MAP.put("userId", Types.BIGINT);
 
 TABLE_COLUMNS_MAP.put("userName", Types.VARCHAR);
 
-TABLE_COLUMNS_MAP.put("createDate", Types.TIMESTAMP);
+TABLE_COLUMNS_MAP.put("layoutSetBranchId", Types.BIGINT);
 
-TABLE_COLUMNS_MAP.put("modifiedDate", Types.TIMESTAMP);
-
-TABLE_COLUMNS_MAP.put("groupId", Types.BIGINT);
+TABLE_COLUMNS_MAP.put("plid", Types.BIGINT);
 
 TABLE_COLUMNS_MAP.put("name", Types.VARCHAR);
 
 TABLE_COLUMNS_MAP.put("description_", Types.VARCHAR);
 
-TABLE_COLUMNS_MAP.put("lastPublishDate", Types.TIMESTAMP);
+TABLE_COLUMNS_MAP.put("master", Types.BOOLEAN);
 
 }
-	public static final String TABLE_SQL_CREATE = "create table Team (mvccVersion LONG default 0 not null,uuid_ VARCHAR(75) null,teamId LONG not null primary key,companyId LONG,userId LONG,userName VARCHAR(75) null,createDate DATE null,modifiedDate DATE null,groupId LONG,name VARCHAR(75) null,description_ STRING null,lastPublishDate DATE null)";
+	public static final String TABLE_SQL_CREATE = "create table LayoutBranch (mvccVersion LONG default 0 not null,layoutBranchId LONG not null primary key,groupId LONG,companyId LONG,userId LONG,userName VARCHAR(75) null,layoutSetBranchId LONG,plid LONG,name VARCHAR(75) null,description_ STRING null,master BOOLEAN)";
 
-	public static final String TABLE_SQL_DROP = "drop table Team";
+	public static final String TABLE_SQL_DROP = "drop table LayoutBranch";
 
 	public static final String[] TABLE_SQL_ADD_INDEXES = {
-		"create unique index IX_143DC786 on Team (groupId, name[$COLUMN_LENGTH:75$])",
-		"create index IX_5D47F637 on Team (uuid_[$COLUMN_LENGTH:75$], companyId)",
-		"create unique index IX_39F69E79 on Team (uuid_[$COLUMN_LENGTH:75$], groupId)"
+		"create index IX_A705FF94 on LayoutBranch (layoutSetBranchId, plid, master)",
+		"create unique index IX_FD57097D on LayoutBranch (layoutSetBranchId, plid, name[$COLUMN_LENGTH:75$])"
 	};
 
 }
