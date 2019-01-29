55bc2355910aad9a97fd750d6da27cc15e3e0ff8
tina.tian <tina.tian@liferay.com>
2019-01-29 11:16:38 -0800

    LPS-89456 Add upgrade process - layout-page-template-service

--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/upgrade/LayoutPageTemplateServiceUpgrade.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/upgrade/LayoutPageTemplateServiceUpgrade.java
@@ -20,6 +20,7 @@ import com.liferay.layout.page.template.internal.upgrade.v1_2_0.UpgradeLayoutPag
 import com.liferay.layout.page.template.internal.upgrade.v2_0_0.util.LayoutPageTemplateCollectionTable;
 import com.liferay.layout.page.template.internal.upgrade.v2_0_0.util.LayoutPageTemplateEntryTable;
 import com.liferay.layout.page.template.internal.upgrade.v2_1_0.UpgradeSchema;
+import com.liferay.layout.page.template.internal.upgrade.v2_1_1.UpgradeBadColumnNames;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.service.LayoutPrototypeLocalService;
@@ -60,6 +61,8 @@ public class LayoutPageTemplateServiceUpgrade
 				}));
 
 		registry.register("2.0.0", "2.1.0", new UpgradeSchema());
+
+		registry.register("2.0.0", "2.1.0", new UpgradeBadColumnNames());
 	}
 
 	@Reference
