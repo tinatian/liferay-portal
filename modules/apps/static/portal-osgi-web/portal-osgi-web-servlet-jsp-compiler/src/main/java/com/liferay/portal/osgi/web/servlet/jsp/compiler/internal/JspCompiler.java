4438170e30c6ecc6752a6a386e5078498c25a38e
Dante Wang <dante.wang@liferay.com>
2019-02-28 11:24:08 +0800

    LPS-91267 Remove logic in setClassPath() since no one is using the classpath passed in by Jasper

--- modules/apps/static/portal-osgi-web/portal-osgi-web-servlet-jsp-compiler/src/main/java/com/liferay/portal/osgi/web/servlet/jsp/compiler/internal/JspCompiler.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-servlet-jsp-compiler/src/main/java/com/liferay/portal/osgi/web/servlet/jsp/compiler/internal/JspCompiler.java
@@ -75,7 +75,6 @@ import org.apache.jasper.Options;
 import org.apache.jasper.compiler.ErrorDispatcher;
 import org.apache.jasper.compiler.JavacErrorDetail;
 import org.apache.jasper.compiler.JspRuntimeContext;
-import org.apache.jasper.compiler.JspUtil;
 import org.apache.jasper.compiler.Jsr199JavaCompiler;
 import org.apache.jasper.compiler.Node;
 
@@ -335,18 +334,6 @@ public class JspCompiler extends Jsr199JavaCompiler {
 
 	@Override
 	public void setClassPath(List<File> classPath) {
-		List<String> paths = new ArrayList<>();
-
-		for (File file : classPath) {
-			paths.add(file.toString());
-		}
-
-		List<String> files = JspUtil.expandClassPath(paths);
-		this.classPath = new ArrayList<>();
-
-		for (String file : files) {
-			this.classPath.add(new File(file));
-		}
 	}
 
 	@Override
@@ -524,7 +511,6 @@ public class JspCompiler extends Jsr199JavaCompiler {
 
 	protected CharArrayWriter charArrayWriter;
 	protected List<BytecodeFile> classFiles;
-	protected List<File> classPath;
 	protected ErrorDispatcher errDispatcher;
 	protected String javaEncoding;
 	protected String javaFileName;
