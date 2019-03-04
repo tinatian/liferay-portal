2811744effc9057c2c47bc175e2a0077af097001
Lance Ji <zheyi.ji@liferay.com>
2019-03-04 14:39:26 -0800

    LPS-91335 Remove deprecated and unused construtors, keep these still in use ones

--- portal-kernel/src/com/liferay/portal/kernel/exception/UserIdException.java
+++ portal-kernel/src/com/liferay/portal/kernel/exception/UserIdException.java
@@ -22,35 +22,8 @@ import com.liferay.portal.kernel.util.StringUtil;
  */
 public class UserIdException extends PortalException {
 
-	/**
-	 * @deprecated As of Wilberforce (7.0.x), replaced by the inner classes
-	 */
-	@Deprecated
-	public UserIdException() {
-	}
-
-	/**
-	 * @deprecated As of Wilberforce (7.0.x), replaced by the inner classes
-	 */
-	@Deprecated
 	public UserIdException(String msg) {
 		super(msg);
-	}
-
-	/**
-	 * @deprecated As of Wilberforce (7.0.x), replaced by the inner classes
-	 */
-	@Deprecated
-	public UserIdException(String msg, Throwable cause) {
-		super(msg, cause);
-	}
-
-	/**
-	 * @deprecated As of Wilberforce (7.0.x), replaced by the inner classes
-	 */
-	@Deprecated
-	public UserIdException(Throwable cause) {
-		super(cause);
 	}
 
 	public static class MustNotBeNull extends UserIdException {
