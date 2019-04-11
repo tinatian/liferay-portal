1a0a7dbf55b3b08defe4ec4ccc09319d06221788
tina.tian <tina.tian@liferay.com>
2019-04-11 14:48:22 -0700

    LPS-93870 Remove deprecated static methods since package major version is bumped, though this is deprecated from 7.1

--- portal-kernel/src/com/liferay/portal/kernel/security/permission/PermissionThreadLocal.java
+++ portal-kernel/src/com/liferay/portal/kernel/security/permission/PermissionThreadLocal.java
@@ -35,16 +35,6 @@ public class PermissionThreadLocal {
 		return _addResource.get();
 	}
 
-	/**
-	 * @deprecated As of Judson (7.1.x), with no direct replacement
-	 */
-	@Deprecated
-	public static boolean isFlushResourceBlockEnabled(
-		long companyId, long groupId, String name) {
-
-		return false;
-	}
-
 	public static boolean isFlushResourcePermissionEnabled(
 		String resourceName, String primKey) {
 
