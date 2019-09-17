50195343d36a8431253782186221fa9b713430b5
Dante Wang <dante.wang@liferay.com>
2019-09-17 13:12:34 +0800

    LPS-91105 Set java compiler options in init()

--- modules/apps/static/portal-osgi-web/portal-osgi-web-servlet-jsp-compiler/src/main/java/com/liferay/portal/osgi/web/servlet/jsp/compiler/internal/JspCompiler.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-servlet-jsp-compiler/src/main/java/com/liferay/portal/osgi/web/servlet/jsp/compiler/internal/JspCompiler.java
@@ -175,7 +175,29 @@ public class JspCompiler {
 
 		_compilerOptions.add("-XDuseUnsharedTable");
 
+		_compilerOptions.add("-proc:none");
+
+		String extDirs = System.getProperty("java.ext.dirs");
+
+		if (extDirs != null) {
+			_compilerOptions.add("-extdirs");
+			_compilerOptions.add(extDirs);
+		}
+
 		Options options = jspCompilationContext.getOptions();
+
+		if (options.getClassDebugInfo()) {
+			_compilerOptions.add("-g");
+		}
+		else {
+			_compilerOptions.add("-g:none");
+		}
+
+		_compilerOptions.add("-source");
+		_compilerOptions.add(options.getCompilerSourceVM());
+
+		_compilerOptions.add("-target");
+		_compilerOptions.add(options.getCompilerTargetVM());
 
 		_classPath.add(options.getScratchDir());
 
@@ -256,8 +278,6 @@ public class JspCompiler {
 		_errorDispatcher = errorDispatcher;
 
 		_jspRuntimeContext = jspCompilationContext.getRuntimeContext();
-
-		_compilerOptions.add("-proc:none");
 	}
 
 	public void release() {
@@ -300,30 +320,6 @@ public class JspCompiler {
 	}
 
 	public void setClassPath(List<File> classPath) {
-	}
-
-	public void setDebug(boolean debug) {
-		if (debug) {
-			_compilerOptions.add("-g");
-		}
-		else {
-			_compilerOptions.add("-g:none");
-		}
-	}
-
-	public void setExtdirs(String exts) {
-		_compilerOptions.add("-extdirs");
-		_compilerOptions.add(exts);
-	}
-
-	public void setSourceVM(String sourceVM) {
-		_compilerOptions.add("-source");
-		_compilerOptions.add(sourceVM);
-	}
-
-	public void setTargetVM(String targetVM) {
-		_compilerOptions.add("-target");
-		_compilerOptions.add(targetVM);
 	}
 
 	protected void addDependenciesToClassPath() {
