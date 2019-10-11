a6903af762cf399888194e1079819845b7be00a0
tina.tian <tina.tian@liferay.com>
2019-10-11 16:10:11 -0700

    LPS-102995 Auto generated

--- modules/apps/change-tracking/change-tracking-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/apps/change-tracking/change-tracking-service/src/main/resources/META-INF/sql/tables.sql
@@ -26,6 +26,15 @@ create table CTEntry (
 	changeType INTEGER
 );
 
+create table CTMessage (
+	mvccVersion LONG default 0 not null,
+	ctMessageId LONG not null primary key,
+	companyId LONG,
+	userId LONG,
+	ctCollectionId LONG,
+	messageContent TEXT null
+);
+
 create table CTPreferences (
 	mvccVersion LONG default 0 not null,
 	ctPreferencesId LONG not null primary key,
