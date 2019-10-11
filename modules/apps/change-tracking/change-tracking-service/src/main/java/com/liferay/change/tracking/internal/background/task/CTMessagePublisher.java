8482b161f382f42d0e7f2679e0518293e8204eec
tina.tian <tina.tian@liferay.com>
2019-10-11 16:30:27 -0700

    LPS-102995 Resend message to message bus when publishing

---
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/background/task/CTMessagePublisher.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.change.tracking.internal.background.task;
+
+import com.liferay.change.tracking.service.CTMessageLocalService;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageBus;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Tina Tian
+ */
+@Component(immediate = true, service = CTMessagePublisher.class)
+public class CTMessagePublisher {
+
+	public void publish(long ctCollectionId) {
+		List<Message> messages = _ctMessageLocalService.getMessages(
+			ctCollectionId);
+
+		for (Message message : messages) {
+			_messageBus.sendMessage(message.getDestinationName(), message);
+		}
+	}
+
+	@Reference
+	private CTMessageLocalService _ctMessageLocalService;
+
+	@Reference
+	private MessageBus _messageBus;
+
+}
