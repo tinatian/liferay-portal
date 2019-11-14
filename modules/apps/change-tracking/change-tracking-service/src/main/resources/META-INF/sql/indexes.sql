76c50679128845ec9dac68dfe81e503a228804fc
tina.tian <tina.tian@liferay.com>
2019-11-13 21:21:00 -0800

    LPS-102995 Auto generate

--- modules/apps/change-tracking/change-tracking-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/change-tracking/change-tracking-service/src/main/resources/META-INF/sql/indexes.sql
@@ -4,6 +4,8 @@ create index IX_8D52E6F9 on CTCollection (companyId, status);
 create unique index IX_295C418C on CTEntry (ctCollectionId, modelClassNameId, modelClassPK);
 create index IX_410E1B23 on CTEntry (ctCollectionId, modelClassPK);
 
+create index IX_9FB742FA on CTMessage (ctCollectionId);
+
 create unique index IX_516E5375 on CTPreferences (companyId, userId);
 create index IX_3FECC82B on CTPreferences (ctCollectionId);
 
