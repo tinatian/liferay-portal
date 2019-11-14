2bf8daa40647299960aa5794a1d86d2cffb2dc46
tina.tian <tina.tian@liferay.com>
2019-11-13 21:18:39 -0800

    LPS-102995 Add support to intercept message sent to MessageBus

--- modules/apps/portal/portal-messaging/src/main/java/com/liferay/portal/messaging/internal/BaseAsyncDestination.java
+++ modules/apps/portal/portal-messaging/src/main/java/com/liferay/portal/messaging/internal/BaseAsyncDestination.java
@@ -148,8 +148,6 @@ public abstract class BaseAsyncDestination extends BaseDestination {
 					"receive more messages"));
 		}
 
-		MessageBusThreadLocalHelper.populateMessageFromThreadLocals(message);
-
 		if (_log.isDebugEnabled()) {
 			_log.debug(
 				StringBundler.concat(
