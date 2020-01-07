0fb0c771bf2ae156634d6c1286f82d957f9eb148
tina.tian <tina.tian@liferay.com>
2020-01-06 17:11:52 -0800

    LPS-103577 portal-search-elasticsearch7: add new properties to configuration for sidecar process

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-api/src/main/java/com/liferay/portal/search/elasticsearch7/configuration/ElasticsearchConfiguration.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-api/src/main/java/com/liferay/portal/search/elasticsearch7/configuration/ElasticsearchConfiguration.java
@@ -164,4 +164,23 @@ public interface ElasticsearchConfiguration {
 	)
 	public String httpCORSConfigurations();
 
+	@Meta.AD(
+		deflt = "10000", description = "sidecar-heartbeat-interval-help",
+		name = "sidecar-heartbeat-interval", required = false
+	)
+	public long sidecarHeartbeatInterval();
+
+	@Meta.AD(
+		deflt = "elasticsearch7", description = "sidecar-home-help",
+		name = "sidecar-home", required = false
+	)
+	public String sidecarHome();
+
+	@Meta.AD(
+		deflt = "-Xms1g|-Xmx1g|-XX:+AlwaysPreTouch",
+		description = "sidecar-jvm-options-help", name = "sidecar-jvm-options",
+		required = false
+	)
+	public String[] sidecarJVMOptions();
+
 }
