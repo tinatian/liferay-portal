66308ee050f53e81a45f599a544385f5d5365502
tina.tian <tina.tian@liferay.com>
2020-06-10 13:16:07 -0700

    LPS-112056 Avoid creating new list when retrieving.

--- portal-kernel/src/com/liferay/portal/kernel/security/permission/ResourceActionsBag.java
+++ portal-kernel/src/com/liferay/portal/kernel/security/permission/ResourceActionsBag.java
@@ -15,6 +15,7 @@
 package com.liferay.portal.kernel.security.permission;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -28,43 +29,49 @@ public class ResourceActionsBag {
 		Set<String> guestDefaultActions, Set<String> guestUnsupportedActions,
 		Set<String> layoutManagerActions, Set<String> ownerDefaultActions) {
 
-		_supportsActions = resourceActions;
-		_groupDefaultActions = groupDefaultActions;
-		_guestDefaultActions = guestDefaultActions;
-		_guestUnsupportedActions = guestUnsupportedActions;
-		_layoutManagerActions = layoutManagerActions;
-		_ownerDefaultActions = ownerDefaultActions;
+		_supportsActions = Collections.unmodifiableList(
+			new ArrayList<>(resourceActions));
+		_groupDefaultActions = Collections.unmodifiableList(
+			new ArrayList<>(groupDefaultActions));
+		_guestDefaultActions = Collections.unmodifiableList(
+			new ArrayList<>(guestDefaultActions));
+		_guestUnsupportedActions = Collections.unmodifiableList(
+			new ArrayList<>(guestUnsupportedActions));
+		_layoutManagerActions = Collections.unmodifiableList(
+			new ArrayList<>(layoutManagerActions));
+		_ownerDefaultActions = Collections.unmodifiableList(
+			new ArrayList<>(ownerDefaultActions));
 	}
 
 	public List<String> getGroupDefaultActions() {
-		return new ArrayList<>(_groupDefaultActions);
+		return _groupDefaultActions;
 	}
 
 	public List<String> getGuestDefaultActions() {
-		return new ArrayList<>(_guestDefaultActions);
+		return _guestDefaultActions;
 	}
 
 	public List<String> getGuestUnsupportedActions() {
-		return new ArrayList<>(_guestUnsupportedActions);
+		return _guestUnsupportedActions;
 	}
 
 	public List<String> getLayoutManagerActions() {
-		return new ArrayList<>(_layoutManagerActions);
+		return _layoutManagerActions;
 	}
 
 	public List<String> getOwnerDefaultActions() {
-		return new ArrayList<>(_ownerDefaultActions);
+		return _ownerDefaultActions;
 	}
 
 	public List<String> getSupportsActions() {
-		return new ArrayList<>(_supportsActions);
+		return _supportsActions;
 	}
 
-	private final Set<String> _groupDefaultActions;
-	private final Set<String> _guestDefaultActions;
-	private final Set<String> _guestUnsupportedActions;
-	private final Set<String> _layoutManagerActions;
-	private final Set<String> _ownerDefaultActions;
-	private final Set<String> _supportsActions;
+	private final List<String> _groupDefaultActions;
+	private final List<String> _guestDefaultActions;
+	private final List<String> _guestUnsupportedActions;
+	private final List<String> _layoutManagerActions;
+	private final List<String> _ownerDefaultActions;
+	private final List<String> _supportsActions;
 
 }
