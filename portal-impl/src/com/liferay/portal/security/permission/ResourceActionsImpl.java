1cc70a109ecdf6b20a7665e5cdc82214589decee
tina.tian <tina.tian@liferay.com>
2020-06-10 14:47:50 -0700

    LPS-112056 Deprecate unused method

--- portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
+++ portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
@@ -101,14 +101,14 @@ public class ResourceActionsImpl implements ResourceActions {
 		}
 	}
 
+	/**
+	 * @deprecated As of Athanasius (7.3.x), replaced by {@link
+	 * 				#check(String)}
+	 */
+	@Deprecated
 	@Override
 	public void check(Portlet portlet) {
-		String portletName = portlet.getPortletId();
-
-		ResourceActionsBag portletResourceActionsBag =
-			_getPortletResourceActionsBag(portletName, portlet);
-
-		_check(portletName, portletResourceActionsBag.getSupportsActions());
+		check(portlet.getPortletName());
 	}
 
 	@Override
