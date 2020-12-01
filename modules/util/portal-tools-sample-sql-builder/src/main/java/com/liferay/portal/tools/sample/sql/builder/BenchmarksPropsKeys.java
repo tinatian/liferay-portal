89bb820965c44760cc39c8055f9aa764d1203cd6
lily.chi <lily.chi@liferay.com>
2020-12-01 16:49:49 +0800

    LPS-83275 Introduce a property to control enable/disable the search bar in a new site

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/BenchmarksPropsKeys.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/BenchmarksPropsKeys.java
@@ -21,6 +21,9 @@ public interface BenchmarksPropsKeys {
 
 	public static final String DB_TYPE = "sample.sql.db.type";
 
+	public static final String ENABLE_SEARCH_BAR =
+		"sample.sql.enable.search.bar";
+
 	public static final String MAX_ASSET_CATEGORY_COUNT =
 		"sample.sql.max.asset.category.count";
 
