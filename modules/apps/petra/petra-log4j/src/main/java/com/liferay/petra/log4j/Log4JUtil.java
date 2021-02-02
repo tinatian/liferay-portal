28a838b2398976ff27b8863433c31a5fe3fa1ca1
hai.yu <8219754@liferay.com>
2021-02-02 13:24:38 +0800

    LPS-125547 Due to "class" and "name" attribute position change of <appender> in portal-log4j.xml(see the commit 8603d918f1013594c70a1291f5c9e5e64ed9bb83), the current logic is invalid. The commit can remove <appender> tag regardless of attribute position change. In additon, document.asXML() remove the space before "/>" (please refer to org.dom4j.io.XMLWriter.writeEmptyElementClose()). So need to replace '"/>' with '" />'.

--- modules/apps/petra/petra-log4j/src/main/java/com/liferay/petra/log4j/Log4JUtil.java
+++ modules/apps/petra/petra-log4j/src/main/java/com/liferay/petra/log4j/Log4JUtil.java
@@ -95,28 +95,7 @@ public class Log4JUtil {
 			LogManager.getLoggerRepository());
 
 		try {
-			SAXReader saxReader = new SAXReader();
-
-			saxReader.setEntityResolver(
-				new EntityResolver() {
-
-					@Override
-					public InputSource resolveEntity(
-						String publicId, String systemId) {
-
-						if (systemId.endsWith("log4j.dtd")) {
-							return new InputSource(
-								DOMConfigurator.class.getResourceAsStream(
-									"log4j.dtd"));
-						}
-
-						return null;
-					}
-
-				});
-
-			Document document = saxReader.read(
-				new UnsyncStringReader(urlContent));
+			Document document = _getDocument(urlContent);
 
 			Element rootElement = document.getRootElement();
 
@@ -231,6 +210,29 @@ public class Log4JUtil {
 		return unsyncByteArrayOutputStream.toByteArray();
 	}
 
+	private static Document _getDocument(String xml) throws Exception {
+		SAXReader saxReader = new SAXReader();
+
+		saxReader.setEntityResolver(
+			new EntityResolver() {
+
+				@Override
+				public InputSource resolveEntity(
+					String publicId, String systemId) {
+
+					if (systemId.endsWith("log4j.dtd")) {
+						return new InputSource(
+							Level.class.getResourceAsStream("xml/log4j.dtd"));
+					}
+
+					return null;
+				}
+
+			});
+
+		return saxReader.read(new UnsyncStringReader(xml));
+	}
+
 	private static java.util.logging.Level _getJdkLevel(String priority) {
 		if (StringUtil.equalsIgnoreCase(priority, Level.DEBUG.toString())) {
 			return java.util.logging.Level.FINE;
@@ -283,16 +285,33 @@ public class Log4JUtil {
 	}
 
 	private static String _removeAppender(String content, String appenderName) {
-		int x = content.indexOf("<appender name=\"" + appenderName + "\"");
+		String oldContent = content;
 
-		int y = content.indexOf("</appender>", x);
+		try {
+			Document document = _getDocument(content);
 
-		if (y != -1) {
-			y = content.indexOf("<", y + 1);
+			Element rootElement = document.getRootElement();
+
+			List<Element> appenderElements = rootElement.elements("appender");
+
+			for (Element appenderElement : appenderElements) {
+				String name = appenderElement.attributeValue("name");
+
+				if (name.equals(appenderName)) {
+					rootElement.remove(appenderElement);
+
+					break;
+				}
+			}
+
+			content = document.asXML();
+		}
+		catch (Exception exception) {
+			_log.error(exception, exception);
 		}
 
-		if ((x != -1) && (y != -1)) {
-			content = content.substring(0, x) + content.substring(y);
+		if (!oldContent.equals(content)) {
+			content = StringUtil.replace(content, "\"/>", "\" />");
 		}
 
 		return StringUtil.removeSubstring(
