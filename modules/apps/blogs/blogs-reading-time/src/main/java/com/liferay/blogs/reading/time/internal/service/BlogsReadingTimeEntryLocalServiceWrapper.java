6d443672b07efef06f15c97fbea197835368236d
tina.tian <tina.tian@liferay.com>
2021-02-19 23:29:29 -0800

    LPS-128171 Apply usages

--- modules/apps/blogs/blogs-reading-time/src/main/java/com/liferay/blogs/reading/time/internal/service/BlogsReadingTimeEntryLocalServiceWrapper.java
+++ modules/apps/blogs/blogs-reading-time/src/main/java/com/liferay/blogs/reading/time/internal/service/BlogsReadingTimeEntryLocalServiceWrapper.java
@@ -35,16 +35,6 @@ import org.osgi.service.component.annotations.Reference;
 public class BlogsReadingTimeEntryLocalServiceWrapper
 	extends BlogsEntryLocalServiceWrapper {
 
-	public BlogsReadingTimeEntryLocalServiceWrapper() {
-		super(null);
-	}
-
-	public BlogsReadingTimeEntryLocalServiceWrapper(
-		BlogsEntryLocalService blogsEntryLocalService) {
-
-		super(blogsEntryLocalService);
-	}
-
 	@Override
 	public BlogsEntry addEntry(
 			long userId, String title, String subtitle, String urlTitle,
