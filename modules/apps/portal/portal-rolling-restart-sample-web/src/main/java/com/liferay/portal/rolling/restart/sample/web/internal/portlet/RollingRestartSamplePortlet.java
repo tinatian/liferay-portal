76390dd99631f31bcf13ce7ad96969146bccfcde
Victor Ware <victor.ware@liferay.com>
2021-06-22 16:20:13 -0700

    LRQA-46475 This will be a sample portlet since the portlet itself is not the test

---
+++ modules/apps/portal/portal-rolling-restart-sample-web/src/main/java/com/liferay/portal/rolling/restart/sample/web/internal/portlet/RollingRestartSamplePortlet.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.rolling.restart.sample.web.internal.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.rolling.restart.sample.web.internal.constants.RollingRestartSamplePortletKeys;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Kyle Miho
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=RollingRestartSample",
+		"javax.portlet.init-param.template-path=/META-INF/resources/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + RollingRestartSamplePortletKeys.TEST,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=guest,power-user,user"
+	},
+	service = Portlet.class
+)
+public class RollingRestartSamplePortlet extends MVCPortlet {
+}
