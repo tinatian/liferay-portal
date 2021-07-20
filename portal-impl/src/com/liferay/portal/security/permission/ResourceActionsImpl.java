286c7acb2104045987d59f236198aaf7c82c8926
tina.tian <tina.tian@liferay.com>
2021-07-20 16:34:57 -0700

    LPS-135983 Make sure one portlet can only be associated to one root model resource

--- portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
+++ portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
@@ -1255,7 +1255,19 @@ public class ResourceActionsImpl implements ResourceActions {
 					modelResourceElement.elementText("root"));
 
 				if (root) {
-					_portletRootModelResources.put(portletName, modelName);
+					String originalRootModelName =
+						_portletRootModelResources.putIfAbsent(
+							portletName, modelName);
+
+					if (Validator.isNotNull(originalRootModelName) &&
+						!Objects.equals(originalRootModelName, modelName)) {
+
+						throw new ResourceActionsException(
+							StringBundler.concat(
+								"More than one root model resources are ",
+								"defined for ", portletName, ": ",
+								originalRootModelName, ", ", modelName));
+					}
 				}
 			}
 
