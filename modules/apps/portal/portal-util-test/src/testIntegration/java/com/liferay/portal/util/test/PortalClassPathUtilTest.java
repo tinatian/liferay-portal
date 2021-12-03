2f2613af47f5cb41a57c0702a64f906907d0c775
Julius Lee <meng-huan.lee@liferay.com>
2021-12-02 17:24:01 -0800

    LPS-142626 Added two tests for BootstrapClassPath
    Added to 2 methods to test if bootstrap class path only contains petra packages and their MANIFEST.MF file does not have headers generated from app.bnd in app module.
    
    (cherry picked from commit 792a15226ae08389989f8fab40086553a208ec47)

--- modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/test/PortalClassPathUtilTest.java
+++ modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/test/PortalClassPathUtilTest.java
@@ -17,17 +17,25 @@ package com.liferay.portal.util.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.petra.process.ProcessConfig;
 import com.liferay.petra.string.StringUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.util.PortalClassPathUtil;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.net.URL;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Scanner;
 import java.util.Set;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -44,11 +52,77 @@ public class PortalClassPathUtilTest {
 	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
 		new LiferayIntegrationTestRule();
 
+	public static ProcessConfig processConfig;
+
+	@BeforeClass
+	public static void setUpClass() {
+		processConfig = PortalClassPathUtil.getPortalProcessConfig();
+	}
+
+	@Test
+	public void testBoostrapClassPathManifest() {
+		List<String> boostrapClassPathEntries = StringUtil.split(
+			processConfig.getBootstrapClassPath(), File.pathSeparatorChar);
+
+		Set<String> headerEntries = new HashSet<>();
+
+		for (String bootstrapClassPathEntry : boostrapClassPathEntries) {
+			if (bootstrapClassPathEntry.contains("petra")) {
+				URL url;
+
+				try {
+					url = new URL(
+						"jar:file:" + bootstrapClassPathEntry +
+							"!/META-INF/MANIFEST.MF");
+
+					InputStream urlInputStream = url.openStream();
+
+					Scanner manifestScanner = new Scanner(urlInputStream);
+
+					while (manifestScanner.hasNext()) {
+						String manifestNextLine = manifestScanner.nextLine();
+
+						if (manifestNextLine.contains("Liferay-Releng")) {
+							headerEntries.add(bootstrapClassPathEntry);
+
+							break;
+						}
+					}
+				}
+				catch (IOException ioException) {
+					_log.error(
+						"Unable to resolve local URL from bundle", ioException);
+				}
+			}
+		}
+
+		Assert.assertTrue(
+			"Bootstrap packages containing petra should not contain" +
+				" header entries in their Manifest.MF file: " + headerEntries,
+			headerEntries.isEmpty());
+	}
+
+	@Test
+	public void testBoostrapClassPathPetra() {
+		List<String> boostrapClassPathEntries = StringUtil.split(
+			processConfig.getBootstrapClassPath(), File.pathSeparatorChar);
+
+		Set<String> nonpetraEntries = new HashSet<>();
+
+		for (String bootstrapClassPathEntry : boostrapClassPathEntries) {
+			if (!bootstrapClassPathEntry.contains("petra")) {
+				nonpetraEntries.add(bootstrapClassPathEntry);
+			}
+		}
+
+		Assert.assertTrue(
+			"Bootstrap packages should only contain petra packages: " +
+				nonpetraEntries,
+			nonpetraEntries.isEmpty());
+	}
+
 	@Test
 	public void testGetPortalProcessConfig() {
-		ProcessConfig processConfig =
-			PortalClassPathUtil.getPortalProcessConfig();
-
 		List<String> runtimeClassPathEntries = StringUtil.split(
 			processConfig.getRuntimeClassPath(), File.pathSeparatorChar);
 
@@ -67,5 +141,8 @@ public class PortalClassPathUtilTest {
 				"duplicate entries: " + duplicateEntries,
 			duplicateEntries.isEmpty());
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		PortalClassPathUtilTest.class);
 
 }
