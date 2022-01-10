1a12b5610ebb6baff25aad5fa0c1db6371fb0e08
Jiefeng Wu <jeffwu0724@gmail.com>
2022-01-10 12:38:01 -0800

    LPS-145063 : remove unused properties "verify.patch.levels.disabled". It was only used in MainServlet.java, and was removed based on commit 0a6c878, see LPS-137936.

--- portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -3498,13 +3498,6 @@ public interface PropsKeys {
 	public static final String VERIFY_FREQUENCY = "verify.frequency";
 
 	/**
-	 * @deprecated As of Athanasius (7.3.x), with no direct replacement
-	 */
-	@Deprecated
-	public static final String VERIFY_PATCH_LEVELS_DISABLED =
-		"verify.patch.levels.disabled";
-
-	/**
 	 * @deprecated As of Cavanaugh (7.4.x), with no direct replacement
 	 */
 	@Deprecated
