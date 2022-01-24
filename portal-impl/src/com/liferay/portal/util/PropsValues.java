33b7c5cdcc0c3e0ce1cdb66707da3f2f3ee3bd72
Jiefeng Wu <jeffwu0724@gmail.com>
2022-01-24 11:37:45 -0800

    LPS-145919 - Remove properties "scripting.jruby.compile.mode". It was deleted in commit 6393d1b9415eefb7528154e27e438a3dd182f5f9, see LPS-55505

--- portal-impl/src/com/liferay/portal/util/PropsValues.java
+++ portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -2517,9 +2517,6 @@ public class PropsValues {
 		GetterUtil.getBoolean(
 			PropsUtil.get(PropsKeys.SCHEMA_MODULE_BUILD_AUTO_UPGRADE));
 
-	public static final String SCRIPTING_JRUBY_COMPILE_MODE = PropsUtil.get(
-		PropsKeys.SCRIPTING_JRUBY_COMPILE_MODE);
-
 	public static final int SCRIPTING_JRUBY_COMPILE_THRESHOLD =
 		GetterUtil.getInteger(
 			PropsUtil.get(PropsKeys.SCRIPTING_JRUBY_COMPILE_THRESHOLD), 50);
