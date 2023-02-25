99526d1f5aec82efa89e5d3da59ac40ed8e8c317
Tina Tian <tina.tian@liferay.com>
2023-02-24 16:19:13 -0800

    LPS-176754 Apply to archived modules

--- modules/apps/archived/recent-documents-web/src/main/java/com/liferay/recent/documents/web/internal/messaging/RecentDocumentsMessageListener.java
+++ modules/apps/archived/recent-documents-web/src/main/java/com/liferay/recent/documents/web/internal/messaging/RecentDocumentsMessageListener.java
@@ -15,24 +15,17 @@
 package com.liferay.recent.documents.web.internal.messaging;
 
 import com.liferay.document.library.file.rank.service.DLFileRankLocalService;
+import com.liferay.petra.function.UnsafeRunnable;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
-import com.liferay.portal.kernel.messaging.BaseMessageListener;
-import com.liferay.portal.kernel.messaging.DestinationNames;
-import com.liferay.portal.kernel.messaging.Message;
-import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
-import com.liferay.portal.kernel.scheduler.SchedulerEntry;
-import com.liferay.portal.kernel.scheduler.SchedulerEntryImpl;
+import com.liferay.portal.kernel.scheduler.SchedulerJobConfiguration;
 import com.liferay.portal.kernel.scheduler.TimeUnit;
-import com.liferay.portal.kernel.scheduler.Trigger;
-import com.liferay.portal.kernel.scheduler.TriggerFactory;
-import com.liferay.portal.kernel.util.Props;
+import com.liferay.portal.kernel.scheduler.TriggerConfiguration;
 import com.liferay.recent.documents.web.internal.configuration.RecentDocumentsConfiguration;
 
 import java.util.Map;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
 /**
@@ -40,52 +33,32 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(
 	configurationPid = "com.liferay.recent.documents.web.internal.configuration.RecentDocumentsConfiguration",
-	immediate = true, service = RecentDocumentsMessageListener.class
+	service = SchedulerJobConfiguration.class
 )
-public class RecentDocumentsMessageListener extends BaseMessageListener {
+public class RecentDocumentsMessageListener
+	implements SchedulerJobConfiguration {
 
-	@Activate
-	protected void activate(Map<String, Object> properties) {
-		RecentDocumentsConfiguration recentDocumentsConfiguration =
-			ConfigurableUtil.createConfigurable(
-				RecentDocumentsConfiguration.class, properties);
-
-		Class<?> clazz = getClass();
-
-		String className = clazz.getName();
-
-		Trigger trigger = _triggerFactory.createTrigger(
-			className, className, null, null,
-			recentDocumentsConfiguration.checkFileRanksInterval(),
-			TimeUnit.MINUTE);
-
-		SchedulerEntry schedulerEntry = new SchedulerEntryImpl(
-			className, trigger);
-
-		_schedulerEngineHelper.register(
-			this, schedulerEntry, DestinationNames.SCHEDULER_DISPATCH);
-	}
-
-	@Deactivate
-	protected void deactivate() {
-		_schedulerEngineHelper.unregister(this);
+	@Override
+	public UnsafeRunnable<Exception> getJobExecutor() {
+		return () -> _dLFileRankLocalService.checkFileRanks();
 	}
 
 	@Override
-	protected void doReceive(Message message) throws Exception {
-		_dLFileRankLocalService.checkFileRanks();
+	public TriggerConfiguration getTriggerConfiguration() {
+		return TriggerConfiguration.createTriggerConfiguration(
+			_recentDocumentsConfiguration.checkFileRanksInterval(),
+			TimeUnit.MINUTE);
+	}
+
+	@Activate
+	protected void activate(Map<String, Object> properties) {
+		_recentDocumentsConfiguration = ConfigurableUtil.createConfigurable(
+			RecentDocumentsConfiguration.class, properties);
 	}
 
 	@Reference
 	private DLFileRankLocalService _dLFileRankLocalService;
 
-	@Reference
-	private Props _props;
-
-	@Reference
-	private SchedulerEngineHelper _schedulerEngineHelper;
-
-	@Reference
-	private TriggerFactory _triggerFactory;
+	private RecentDocumentsConfiguration _recentDocumentsConfiguration;
 
 }
--- modules/apps/archived/sync-service/src/main/java/com/liferay/sync/internal/messaging/SyncMaintenanceMessageListener.java
+++ modules/apps/archived/sync-service/src/main/java/com/liferay/sync/internal/messaging/SyncMaintenanceMessageListener.java
@@ -16,21 +16,18 @@ package com.liferay.sync.internal.messaging;
 
 import com.liferay.document.library.sync.model.DLSyncEvent;
 import com.liferay.document.library.sync.service.DLSyncEventLocalService;
+import com.liferay.petra.function.UnsafeRunnable;
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.Property;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.messaging.BaseMessageListener;
 import com.liferay.portal.kernel.messaging.DestinationNames;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageBusUtil;
-import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
-import com.liferay.portal.kernel.scheduler.SchedulerEntry;
-import com.liferay.portal.kernel.scheduler.SchedulerEntryImpl;
+import com.liferay.portal.kernel.scheduler.SchedulerJobConfiguration;
 import com.liferay.portal.kernel.scheduler.TimeUnit;
-import com.liferay.portal.kernel.scheduler.Trigger;
-import com.liferay.portal.kernel.scheduler.TriggerFactory;
+import com.liferay.portal.kernel.scheduler.TriggerConfiguration;
 import com.liferay.portal.kernel.transaction.TransactionCommitCallbackUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.sync.internal.configuration.SyncServiceConfigurationValues;
@@ -38,155 +35,122 @@ import com.liferay.sync.model.SyncDLObject;
 import com.liferay.sync.service.SyncDLFileVersionDiffLocalService;
 import com.liferay.sync.service.SyncDLObjectLocalService;
 
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Dennis Ju
  */
-@Component(immediate = true, service = SyncMaintenanceMessageListener.class)
-public class SyncMaintenanceMessageListener extends BaseMessageListener {
+@Component(service = SchedulerJobConfiguration.class)
+public class SyncMaintenanceMessageListener
+	implements SchedulerJobConfiguration {
 
 	public static final String DESTINATION_NAME =
 		"liferay/sync_maintenance_processor";
 
-	@Activate
-	protected void activate() {
-		Class<?> clazz = getClass();
-
-		String className = clazz.getName();
-
-		Trigger trigger = _triggerFactory.createTrigger(
-			className, className, null, null, 1, TimeUnit.HOUR);
-
-		SchedulerEntry schedulerEntry = new SchedulerEntryImpl(
-			className, trigger);
-
-		_schedulerEngineHelper.register(this, schedulerEntry, DESTINATION_NAME);
-	}
-
-	@Deactivate
-	protected void deactivate() {
-		_schedulerEngineHelper.unregister(this);
+	@Override
+	public String getDestinationName() {
+		return DESTINATION_NAME;
 	}
 
 	@Override
-	protected void doReceive(Message message) throws Exception {
-		_syncDLFileVersionDiffLocalService.
-			deleteExpiredSyncDLFileVersionDiffs();
+	public UnsafeRunnable<Exception> getJobExecutor() {
+		return () -> {
+			_syncDLFileVersionDiffLocalService.
+				deleteExpiredSyncDLFileVersionDiffs();
 
-		if (SyncServiceConfigurationValues.SYNC_FILE_DIFF_CACHE_ENABLED) {
+			if (SyncServiceConfigurationValues.SYNC_FILE_DIFF_CACHE_ENABLED) {
+				try {
+					_syncDLFileVersionDiffLocalService.
+						deleteExpiredSyncDLFileVersionDiffs();
+				}
+				catch (Exception exception) {
+					_log.error(exception);
+				}
+			}
+
 			try {
-				_syncDLFileVersionDiffLocalService.
-					deleteExpiredSyncDLFileVersionDiffs();
+				ActionableDynamicQuery actionableDynamicQuery =
+					_dlSyncEventLocalService.getActionableDynamicQuery();
+
+				actionableDynamicQuery.setAddCriteriaMethod(
+					dynamicQuery -> {
+						Property modifiedTimeProperty =
+							PropertyFactoryUtil.forName("modifiedTime");
+
+						dynamicQuery.add(
+							modifiedTimeProperty.le(
+								_syncDLObjectLocalService.
+									getLatestModifiedTime()));
+					});
+				actionableDynamicQuery.setPerformActionMethod(
+					(DLSyncEvent dlSyncEvent) -> {
+						SyncDLObject syncDLObject =
+							_syncDLObjectLocalService.fetchSyncDLObject(
+								dlSyncEvent.getType(), dlSyncEvent.getTypePK());
+
+						if ((syncDLObject == null) ||
+							(dlSyncEvent.getModifiedTime() >
+								syncDLObject.getModifiedTime())) {
+
+							TransactionCommitCallbackUtil.registerCallback(
+								() -> {
+									Message dlSyncEventMessage = new Message();
+
+									long lastModifiedTime =
+										_syncDLObjectLocalService.
+											getLatestModifiedTime();
+
+									dlSyncEventMessage.setValues(
+										HashMapBuilder.<String, Object>put(
+											"event", dlSyncEvent.getEvent()
+										).put(
+											"modifiedTime", ++lastModifiedTime
+										).put(
+											"type", dlSyncEvent.getType()
+										).put(
+											"typePK", dlSyncEvent.getTypePK()
+										).build());
+
+									MessageBusUtil.sendMessage(
+										DestinationNames.
+											DOCUMENT_LIBRARY_SYNC_EVENT_PROCESSOR,
+										dlSyncEventMessage);
+
+									return null;
+								});
+						}
+						else {
+							_dlSyncEventLocalService.deleteDLSyncEvent(
+								dlSyncEvent);
+						}
+					});
+
+				actionableDynamicQuery.performActions();
 			}
 			catch (Exception exception) {
 				_log.error(exception);
 			}
-		}
-
-		try {
-			ActionableDynamicQuery actionableDynamicQuery =
-				_dlSyncEventLocalService.getActionableDynamicQuery();
-
-			actionableDynamicQuery.setAddCriteriaMethod(
-				dynamicQuery -> {
-					Property modifiedTimeProperty = PropertyFactoryUtil.forName(
-						"modifiedTime");
-
-					dynamicQuery.add(
-						modifiedTimeProperty.le(
-							_syncDLObjectLocalService.getLatestModifiedTime()));
-				});
-			actionableDynamicQuery.setPerformActionMethod(
-				(DLSyncEvent dlSyncEvent) -> {
-					SyncDLObject syncDLObject =
-						_syncDLObjectLocalService.fetchSyncDLObject(
-							dlSyncEvent.getType(), dlSyncEvent.getTypePK());
-
-					if ((syncDLObject == null) ||
-						(dlSyncEvent.getModifiedTime() >
-							syncDLObject.getModifiedTime())) {
-
-						TransactionCommitCallbackUtil.registerCallback(
-							() -> {
-								Message dlSyncEventMessage = new Message();
-
-								long lastModifiedTime =
-									_syncDLObjectLocalService.
-										getLatestModifiedTime();
-
-								dlSyncEventMessage.setValues(
-									HashMapBuilder.<String, Object>put(
-										"event", dlSyncEvent.getEvent()
-									).put(
-										"modifiedTime", ++lastModifiedTime
-									).put(
-										"type", dlSyncEvent.getType()
-									).put(
-										"typePK", dlSyncEvent.getTypePK()
-									).build());
-
-								MessageBusUtil.sendMessage(
-									DestinationNames.
-										DOCUMENT_LIBRARY_SYNC_EVENT_PROCESSOR,
-									dlSyncEventMessage);
-
-								return null;
-							});
-					}
-					else {
-						_dlSyncEventLocalService.deleteDLSyncEvent(dlSyncEvent);
-					}
-				});
-
-			actionableDynamicQuery.performActions();
-		}
-		catch (Exception exception) {
-			_log.error(exception);
-		}
+		};
 	}
 
-	@Reference(unbind = "-")
-	protected void setDLSyncEventLocalService(
-		DLSyncEventLocalService dlSyncEventLocalService) {
-
-		_dlSyncEventLocalService = dlSyncEventLocalService;
-	}
-
-	@Reference(unbind = "-")
-	protected void setSchedulerEngineHelper(
-		SchedulerEngineHelper schedulerEngineHelper) {
-
-		_schedulerEngineHelper = schedulerEngineHelper;
-	}
-
-	@Reference(unbind = "-")
-	protected void setSyncDLFileVersionDiffLocalService(
-		SyncDLFileVersionDiffLocalService syncDLFileVersionDiffLocalService) {
-
-		_syncDLFileVersionDiffLocalService = syncDLFileVersionDiffLocalService;
-	}
-
-	@Reference(unbind = "-")
-	protected void setSyncDLObjectLocalService(
-		SyncDLObjectLocalService syncDLObjectLocalService) {
-
-		_syncDLObjectLocalService = syncDLObjectLocalService;
+	@Override
+	public TriggerConfiguration getTriggerConfiguration() {
+		return TriggerConfiguration.createTriggerConfiguration(
+			1, TimeUnit.HOUR);
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		SyncMaintenanceMessageListener.class);
 
+	@Reference
 	private DLSyncEventLocalService _dlSyncEventLocalService;
-	private SchedulerEngineHelper _schedulerEngineHelper;
-	private SyncDLFileVersionDiffLocalService
-		_syncDLFileVersionDiffLocalService;
-	private SyncDLObjectLocalService _syncDLObjectLocalService;
 
 	@Reference
-	private TriggerFactory _triggerFactory;
+	private SyncDLFileVersionDiffLocalService
+		_syncDLFileVersionDiffLocalService;
+
+	@Reference
+	private SyncDLObjectLocalService _syncDLObjectLocalService;
 
 }
