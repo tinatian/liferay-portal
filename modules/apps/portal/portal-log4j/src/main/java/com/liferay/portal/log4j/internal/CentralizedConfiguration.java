61b8095abe037e8ae37806e553af72ebcfc59f4a
Julius Lee <meng-huan.lee@liferay.com>
2024-01-22 17:07:49 -0800

    LPD-15176 Remove ReflectionUtil usages in CentralizedConfiguration

--- modules/apps/portal/portal-log4j/src/main/java/com/liferay/portal/log4j/internal/CentralizedConfiguration.java
+++ modules/apps/portal/portal-log4j/src/main/java/com/liferay/portal/log4j/internal/CentralizedConfiguration.java
@@ -5,13 +5,8 @@
 
 package com.liferay.portal.log4j.internal;
 
-import com.liferay.petra.reflect.ReflectionUtil;
-
 import java.io.File;
 
-import java.lang.reflect.Field;
-
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -214,15 +209,8 @@ public class CentralizedConfiguration extends AbstractConfiguration {
 
 		Map<String, Appender> newAppenders = newLoggerConfig.getAppenders();
 
-		List<AppenderRef> currentAppenderRefs = new ArrayList<>(
-			currentLoggerConfig.getAppenderRefs());
-
-		try {
-			_appenderRefsField.set(currentLoggerConfig, currentAppenderRefs);
-		}
-		catch (IllegalAccessException illegalAccessException) {
-			ReflectionUtil.throwException(illegalAccessException);
-		}
+		List<AppenderRef> currentAppenderRefs =
+			currentLoggerConfig.getAppenderRefs();
 
 		for (Appender newAppender : newAppenders.values()) {
 			String name = newAppender.getName();
@@ -257,18 +245,6 @@ public class CentralizedConfiguration extends AbstractConfiguration {
 
 				currentAppenderRefs.add(newAppenderRef);
 			}
-		}
-	}
-
-	private static final Field _appenderRefsField;
-
-	static {
-		try {
-			_appenderRefsField = ReflectionUtil.getDeclaredField(
-				LoggerConfig.class, "appenderRefs");
-		}
-		catch (Exception exception) {
-			throw new ExceptionInInitializerError(exception);
 		}
 	}
 
