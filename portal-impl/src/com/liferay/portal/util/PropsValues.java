34b8ef240d0761f09cf05e619d1d98c65eb94752
Tina Tian <tina.tian@liferay.com>
2024-01-25 10:40:24 -0800

    LPD-15514 Unfinal PropsValues.USERS_EXPORT_CSV_FIELDS to make ExportUsersMVCResourceCommandTest work on JDK17 and higher

--- portal-impl/src/com/liferay/portal/util/PropsValues.java
+++ portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -2415,7 +2415,7 @@ public class PropsValues {
 	public static boolean USERS_EMAIL_ADDRESS_REQUIRED = GetterUtil.getBoolean(
 		PropsUtil.get(PropsKeys.USERS_EMAIL_ADDRESS_REQUIRED));
 
-	public static final String[] USERS_EXPORT_CSV_FIELDS = PropsUtil.getArray(
+	public static String[] USERS_EXPORT_CSV_FIELDS = PropsUtil.getArray(
 		PropsKeys.USERS_EXPORT_CSV_FIELDS);
 
 	public static final String USERS_PROFILE_FRIENDLY_URL = PropsUtil.get(
