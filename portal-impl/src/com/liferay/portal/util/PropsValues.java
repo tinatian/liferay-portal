7b85eadfe69e94780bb3032364f44aa038628e80
Tina Tian <tina.tian@liferay.com>
2024-01-25 10:37:44 -0800

    LPD-15514 Unfinal PropsValues.COMPANY_DEFAULT_HOME_URL to make VirtualHostFilterTest work on JDK17 and higher

--- portal-impl/src/com/liferay/portal/util/PropsValues.java
+++ portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -341,7 +341,7 @@ public class PropsValues {
 	public static final int COMBO_MAX_FILES = GetterUtil.getInteger(
 		PropsUtil.get(PropsKeys.COMBO_MAX_FILES), -1);
 
-	public static final String COMPANY_DEFAULT_HOME_URL = PropsUtil.get(
+	public static String COMPANY_DEFAULT_HOME_URL = PropsUtil.get(
 		PropsKeys.COMPANY_DEFAULT_HOME_URL);
 
 	public static String COMPANY_DEFAULT_LOCALE = PropsUtil.get(
